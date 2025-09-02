db36e04fa90d1696d30f6d9a99f50737a8d927a9
grethaflorian <floriangretha@gmail.com>
2025-09-02 16:46:01 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -5,11 +5,42 @@ es = 1;
 imax = 20;
 % nao alterar: fim
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+% === Metodo de Newton: requer func e func_d; es em fracao (1%) ===
+t = zeros(imax, 1);
+erro = zeros(imax, 1);
 
-% digite seu codigo aqui
-t = 0; % alterar
+t(1) = x0;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+for ii = 1:imax-1
+  den = func_d(t(ii));
+  if den == 0
+    % derivada nula: retorna melhor estimativa conhecida
+    t = t(ii);
+    break
+  endif
+
+  t(ii+1) = t(ii) - func(t(ii)) / den;
+
+  if t(ii+1) ~= 0
+    erro(ii+1) = abs((t(ii+1) - t(ii)) / t(ii+1));  % erro em fracao
+  else
+    erro(ii+1) = Inf;
+  endif
+
+  if erro(ii+1) < es
+    t = t(ii+1);   % convergiu
+    break
+  endif
+
+  % continua ate imax
+endfor
+
+% se nao convergiu, devolve a ultima aproximacao calculada
+if length(t) > 1 && t(end) ~= 0
+  t = t(find(t, 1, "last"));
+else
+  t = t(end);
+endif
+
 
 endfunction
